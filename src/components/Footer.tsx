c3ad4cecac1dd254aa4c501330d1ae914655c1bc
Saroj Niraula <saroj.niraula@scsoftware.fi>
2025-08-14 10:08:28 +0300

    new window

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <p>&copy; 2024 Hamro Sahakarya Samuha</p>
             <p>
                 Follow us:
-                <a href="https://www.facebook.com/people/Hamro-Sahakarya-Samuha/61567535194163/#">Facebook</a> | <a href="#">Twitter</a> | <a href="#">Instagram</a>
+                <a href="https://www.facebook.com/people/Hamro-Sahakarya-Samuha/61567535194163/#" target="_blank">Facebook</a> | <a href="#">Twitter</a> | <a href="#">Instagram</a>
             </p>
         </footer>
     );
