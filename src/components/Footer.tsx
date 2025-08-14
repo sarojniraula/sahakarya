260f6aacfa75b5356df72459bedbd87f51125888
Saroj Niraula <saroj.niraula@scsoftware.fi>
2025-08-14 10:23:02 +0300

    facebook link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,22 @@ function Footer() {
             <p>&copy; 2024 Hamro Sahakarya Samuha</p>
             <p>
                 Follow us:
-                <a href="https://www.facebook.com/people/Hamro-Sahakarya-Samuha/61567535194163/#" target="_blank">Facebook</a> | <a href="#">Twitter</a> | <a href="#">Instagram</a>
+                <a href="fb://page/61567535194163"
+                    onClick={() => {
+                        // Open a blank tab immediately (browser allows it because it's in click event)
+                        const newTab = window.open("", "_blank");
+
+                        // After short delay, set it to the FB web URL
+                        setTimeout(() => {
+                            if (newTab) {
+                                newTab.location.href =
+                                    "https://www.facebook.com/people/Hamro-Sahakarya-Samuha/61567535194163/";
+                            }
+                        }, 500);
+                    }}
+                >
+                    Facebook
+                </a> | <a href="#">Twitter</a> | <a href="#">Instagram</a>
             </p>
         </footer>
     );
